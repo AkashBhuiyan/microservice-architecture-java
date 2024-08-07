3e4801906680594cfe3c5f4f4706f67db7bd8fb7
Akash <akash.bhuiyan@gmail.com>
2024-08-07 22:07:11 +0600

    Setup Service Discovery agent using Eureka server for service registry.

---
+++ eurekaserver/src/test/java/com/akash/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
