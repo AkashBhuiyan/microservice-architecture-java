f85da473e4e9e141480c62fdd3c7d4ad2fa026f9
Akash <akash.bhuiyan@gmail.com>
2024-11-09 15:40:02 +0600

    add fallback method for getJavaVersion endpoint with default Java version
    - Added getJavaVersionFallback to handle failures in retrieving Java version
    - Returns HTTP 200 OK with default Java version "Java 21" when fallback is triggered
    - ratelimiter configuration added for accounts microservice.

--- accounts/src/main/java/com/akash/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/akash/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.akash.accounts.dto.CustomerDto;
 import com.akash.accounts.dto.ErrorResponseDto;
 import com.akash.accounts.dto.ResponseDto;
 import com.akash.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -234,11 +235,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 21");
     }
 
     @Operation(
