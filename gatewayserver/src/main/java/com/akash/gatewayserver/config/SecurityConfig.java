6445b393e12d97431618fa7c0ba54d2ee9e75d33
Akash <akash.bhuiyan@gmail.com>
2025-01-26 19:11:39 +0100

    implement Keycloak role-based authorization for API endpoints
    - Added `KeycloakRoleConverter` to map Keycloak roles to Spring Security authorities.
    - Configured `ServerHttpSecurity` to authorize API access based on roles:
      - GET requests: Permit all.
      - `/bank/accounts/**`: Requires role ACCOUNTS.
      - `/bank/cards/**`: Requires role CARDS.
      - `/bank/loans/**`: Requires role LOANS.
    - Integrated the custom `KeycloakRoleConverter` with the JWT authentication flow.

--- gatewayserver/src/main/java/com/akash/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/akash/gatewayserver/config/SecurityConfig.java
@@ -2,11 +2,16 @@ package com.akash.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 /***
  * Akash Bhuiyan, 27/11/24
@@ -19,12 +24,20 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
-                        .pathMatchers("/bank/accounts/**").authenticated()
-                        .pathMatchers("/bank/cards/**").authenticated()
-                        .pathMatchers("/bank/loans/**").authenticated())
+                        .pathMatchers("/bank/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/bank/cards/**").hasRole("CARDS")
+                        .pathMatchers("/bank/loans/**").hasRole("LOANS"))
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
-                        .jwt(Customizer.withDefaults()));
-        serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
+                        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
+        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
         return serverHttpSecurity.build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+                (new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 }
